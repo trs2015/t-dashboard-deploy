c50e0170a3a2b3cab42090eeb442551df02be2e1
anatoliy <anatoliynbulyga@gmail.com>
2024-09-10 18:26:57 +0300

    fix origin path

--- api/index.js
+++ api/index.js
@@ -17,7 +17,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 const corsOptions = {
-    origin: 'http://localhost:5173',
+    origin: process.env.CLIENT_URL,
     credentials: true,
     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 }
